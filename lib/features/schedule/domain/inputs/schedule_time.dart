41627d9193130d1846b2ce75e814263ef9419439
Cesar Tovar <cjataonline@gmail.com>
2024-04-21 03:26:17 -0400

    agregue la validacion de canchas cerradas

--- lib/features/schedule/domain/inputs/schedule_time.dart
+++ lib/features/schedule/domain/inputs/schedule_time.dart
@@ -3,7 +3,8 @@
   import 'package:flutter/material.dart';
 import 'package:formz/formz.dart';
 
-enum ScheduleTimeError { invalid }
+enum ScheduleTimeError { invalid, isClosed }
+const timeExcludes = [0,1,2,3,4,5,6,22,23];
 
   class ScheduleTime extends FormzInput< TimeOfDay?, ScheduleTimeError > {
   
@@ -16,6 +17,7 @@ enum ScheduleTimeError { invalid }
     if( isValid || isPure ) return null;
 
     if( displayError == ScheduleTimeError.invalid ) return 'Hora requerida';
+    if( displayError == ScheduleTimeError.isClosed ) return 'Horario no permitido';
 
     return null;
   }
@@ -24,6 +26,9 @@ enum ScheduleTimeError { invalid }
   @override
   ScheduleTimeError? validator(TimeOfDay? value) {
     if( value == null || value == TimeOfDay.now() ) return ScheduleTimeError.invalid;
+    for( int i = 0; i < timeExcludes.length; i++){
+      if( value.hour == timeExcludes[i] ) return ScheduleTimeError.isClosed;
+    }
     return null;
   }
 

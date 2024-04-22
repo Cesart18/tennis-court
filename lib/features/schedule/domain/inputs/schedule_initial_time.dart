8a2c1d81743455c6c5a0c0779d1720c1eca3503d
Cesar Tovar <cjataonline@gmail.com>
2024-04-22 00:00:26 -0400

    ...

--- lib/features/schedule/domain/inputs/schedule_initial_time.dart
+++ lib/features/schedule/domain/inputs/schedule_initial_time.dart
@@ -26,7 +26,7 @@ const timeExcludes = [0,1,2,3,4,5,6,22,23];
 
   @override
   ScheduleInitialTimeError? validator(TimeOfDay? value) {
-    if( value == null || value == TimeOfDay.now() ) return ScheduleInitialTimeError.invalid;
+    if( value == null ) return ScheduleInitialTimeError.invalid;
     for( int i = 0; i < timeExcludes.length; i++){
       if( value.hour == timeExcludes[i] ) return ScheduleInitialTimeError.isClosed;
     }

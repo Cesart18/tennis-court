170e1d82e50ec13dd095026863043290a92cb68e
Cesar Tovar <cjataonline@gmail.com>
2024-04-22 10:19:53 -0400

    correccion de errores y validaciones

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:tennis_court/config/config.dart';
 import 'package:tennis_court/shared/shared.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 
 void main() async {
@@ -26,6 +27,16 @@ class MyApp extends ConsumerWidget {
       title: 'Tennis Court',
       theme: AppTheme(isDarkMode: isDarkMode).getTheme(),
       routerConfig: appRouter,
+
+      localizationsDelegates: const [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ],
+  supportedLocales: const [
+    Locale('en'), // English
+    Locale('es'), // Spanish
+  ],
 
     );
   }
